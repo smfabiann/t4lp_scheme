569d284cbda35153f30bc0b6fc23c9a89291dcd4
smfabiann <smfabiann@gmail.com>
2025-05-30 21:56:07 +0000

    finalmente termine esta parte de mierda

--- P4.rkt
+++ P4.rkt
@@ -1,53 +1,48 @@
 #lang scheme
 
-;; Busca las rutas para encontrar un producto 'producto' en un arbol N-ario (lista compuesta) 'arbol'.
-;; Retorna las rutas en forma de listas reprecentado los indices de la lista compuesta
+;; Funcion que busca todas las rutas hacia un producto en una bodega modelada como arbol N-ario.
+;; Cada sublista representa un pasillo/estante y los simbolos son productos.
+;; Retorna una lista de rutas (listas de indices) desde la entrada hasta cada aparicion del producto buscado.
 ;;
-;; arbol : lista compuesta reprecentando el arbol N-ario
-;; producto : elemento a buscar en el arbol
+;; arbol : lista anidada que representa la bodega (el primer elemento es el nombre de la bodega)
+;; producto : simbolo que indica el producto a buscar
 (define (busqueda-almacen arbol producto)
-    ;; Misma funcionalidad que 'bisqueda-almacen' pero con la capasidad de guardar el recorrido, asi
-    ;; poder usar recursividad mientras se guarda el recorrido de la busqueda.
+    ;; Funcion auxiliar recursiva que busca el producto en el contenido actual y acumula la ruta recorrida.
     ;;
-    ;; arbol : lista compuesta reprecentando el arbol N-ario
-    ;; producto : elemento a buscar en el arbol
-    ;; recorrido : lista que guarda el recorrido de la busqueda
-    (define (auxiliar ls producto recorrido)
-        (if (null? ls)   ;; iteramos
-            '()
-            (let ((i (+ 1 (length recorrido))))   ;; creamos la variable i
-                (if (list? (car ls))
-                    ;; si es una lista, seguimos buscando
-                    (append
-                        (auxiliar (car ls) producto (append recorrido (list i)))  
-                        (auxiliar (cdr ls) producto recorrido)  ;; continuamos con el resto de la lista
-                    )
-                    ;; sino, vemos si encontramos el producto
-                    (if (eq? (car ls) producto)  
-                        ;; si encontramos el producto
-                        (cons (append recorrido (list i))  ;; guardamos el recorrido
-                                (auxiliar (cdr ls) producto recorrido))  ;; continuamos con el resto de la lista
-                        ;; si no es el producto
-                        (auxiliar (cdr ls) producto recorrido)  ;; seguimos buscando
-                    )
-                )
-            )  ;; 
-        )
-    )
-    ;; partimos saltando el primer elemento "bodega"
-    (auxiliar (cdr arbol) producto '())
-)
+    ;; contenido : lista actual a recorrer (puede ser una sublista de la bodega)
+    ;; ruta : lista de indices que representa el camino recorrido hasta el punto actual
+    (define (buscar contenido ruta)
+        ;; Funcion interna que recorre la lista actual guardando las rutas que se encuentran.
+        ;;
+        ;; cont : lista a recorrer en este nivel
+        ;; idx : indice actual (empieza en 1)
+        ;; acc : acumulador de rutas encontradas
+        (define (iter cont idx acc)
+            (if (null? cont)
+                acc
+                (let ((elem (car cont)))
+                    (if (list? elem)
+                        ;; Si se ecnuentra una lista, entra a la sublista (car elem)y se pasa (cdr elem)
+                        (iter (cdr cont) (+ idx 1)
+                               (append acc (buscar (cdr elem) (append ruta (list idx)))))
+                        ;; Si se encuentra el producto buscado, se agrega la ruta
+                        (if (eq? elem producto)
+                            (iter (cdr cont) (+ idx 1)
+                                  (append acc (list (append ruta (list idx)))))
+                            (iter (cdr cont) (+ idx 1) acc))))))
+        ;; Se empueza el recorrido desde el indice 1, porque en el 0 esta 'bodega'
+        (iter contenido 1 '()))
+    ;; Se ignora 'bodega' y se empieza a buscar el producto en el arbol
+    (buscar (cdr arbol) '()))
 
-
+;; Definicion de la bodega de prueba (arbol N-ario con pasillos, estantes y productos)
 (define bodega
-'(bodega
-(pasillo1 leche pan)
-(pasillo2 (estanteA pan queso) (estanteB manzana pan))
-(pasillo3 fruta)))
+  '(bodega
+    (pasillo1 leche pan)
+    (pasillo2 (estanteA pan queso) (estanteB manzana pan))
+    (pasillo3 fruta)))
 
-;; 1. Buscar “pan”
+;; Pruebas de la funcion con distintos productos
 (busqueda-almacen bodega 'pan)
-;; 2. Buscar “queso”
 (busqueda-almacen bodega 'queso)
-;; 3. Buscar “fruta”
 (busqueda-almacen bodega 'fruta)
