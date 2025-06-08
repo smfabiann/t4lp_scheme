072305cc59396a7583749f7c56d55a1d5a5aeb1c
smfabiann <smfabiann@gmail.com>
2025-06-08 22:27:07 +0000

    cambios pequeños

--- P1.rkt
+++ P1.rkt
@@ -2,15 +2,15 @@
 
 ;; retorna true cuando la longitud de las 'lista' es la misma que el numero 'n'
 ;;
-;; numero : int a comparar con la longitud de la lista
-;; lista : la lista con la que se compara n
-(define (coincide? numero lista)
-    (if (null? lista)
-        (if (= numero 0)    ;; se verifica si el numero es 0, caso para detener la recursión
+;; n : int a comparar con la longitud de la lista
+;; ls : la lista con la que se compara n
+(define (coincide? n ls)
+    (if (null? ls)
+        (if (= n 0)    ;; se verifica si el n es 0, caso para detener la recursión
             true    ;; si es 0, se devuelve true, significando que hay una misma cantidad e elementos
-            false)  ;; si se agora la lista y el numero no es 0, se devuelve false, significando que no tienen la misma cantidad
-        ;; se le resta 1 al numero y se devuelve la lista sin el primer elemento
-        (coincide? (- numero 1) (cdr lista)) ;; Recurcion en cola
+            false)  ;; si se agora la lista y el n no es 0, se devuelve false, significando que no tienen la misma cantidad
+        ;; se le resta 1 al n y se devuelve la lista sin el primer elemento
+        (coincide? (- n 1) (cdr ls)) ;; Recurcion en cola
     )
 )
 
