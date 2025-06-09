11fc25ce2d2820961dca12bb4bf7374e43fb3304
smfabiann <smfabiann@gmail.com>
2025-06-09 16:49:33 +0000

    se agregaron comentarios xd

--- P1.rkt
+++ P1.rkt
@@ -1,12 +1,18 @@
 #lang scheme
 
-;; funcion auxiliar de coinside
-;; da la longitud de una lista
+;; Funcion auxiliar de coincide?.
+;; Retorna una cantidad de elementos en la lista ls.
+;;
+;; ls : lista cuyos elementos se van a contar
 (define (longa ls)
     (if (null? ls)
         0
         (+ 1 (longa (cdr ls)))))
 
+;; Funcion que compara el largo de una lista "ls" con una cantidad "n".
+;;
+;; n : numero a comparar con la longitud de la lista
+;; ls : lista cuyos elementos se van a contar
 (define (coincide? n ls)
     (if (eq? n (longa ls))
         true
